d7902a4814c3f2c88c4ba6dcbc1ebfd337a539c7
James Britton <brittonjp@gmail.com>
2019-03-14 14:40:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,6 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  end
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   end
